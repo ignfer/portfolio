e5310e16d4934196415ff5841f40eb77dd4dc63d
Ignacio Fernández <nachofernandezuru@gmail.com>
2023-09-27 23:21:28 -0300

    Update
    - Created some needed files for new components
    - Defined some color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { Aside } from './components/Aside/Aside.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <header>
+
+      </header>
+      <body>
+        
+      </body>
     </>
   )
 }
---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,13 @@
+import './Aside.css'
+
+function Aside(){
+    return(
+        <>
+            <aside>
+                
+            </aside>
+        </>
+    );
+}
+
+export {Aside};
