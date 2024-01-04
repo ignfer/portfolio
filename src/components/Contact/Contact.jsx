e1d51e0b6d024611c8b2b6052d845aba2b8818af
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-01-04 16:01:19 -0300

    Upload
    - added redirects when click on the contacts icons
    - uploaded the cv to redirect to it later on

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,9 +5,15 @@ export default function Contact(){
       <div className='contact'>
         <p>contact me</p>
         <div className='contact-images-container'>
-          <img width="42" height="42" src={'./assets/svgs/github.svg'} alt='Visit my GitHub'></img>
-          <img width="42" height="42" src={'./assets/svgs/linkedin.svg'} alt='Visit my Linkedin'></img>
-          <img width="35" height="35" src={'./assets/svgs/resume.svg'} alt='Download my Resume'></img>
+          <a href='https://github.com/ignfer' target='_blank' rel='noreferrer'>
+            <img width="42" height="42" src={'./assets/svgs/github.svg'} alt='Visit my GitHub'></img>
+          </a>
+          <a href='www.linkedin.com/in/ignaciofernández' target='_blank' rel='noreferrer'>
+            <img width="42" height="42" src={'./assets/svgs/linkedin.svg'} alt='Visit my Linkedin'></img>
+          </a>
+          <a href='https://github.com/ignfer' target='_blank' rel='noreferrer'>
+            <img width="35" height="35" src={'./assets/svgs/resume.svg'} alt='Download my Resume'></img>
+          </a>
         </div>
       </div>
     </>
