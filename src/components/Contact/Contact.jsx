8a3568b0541df14252f2247e2736db5df0ab2f74
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-01-04 15:53:11 -0300

    Upload
    - added new svgs instead of png's icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,9 +5,9 @@ export default function Contact(){
       <div className='contact'>
         <p>contact me</p>
         <div className='contact-images-container'>
-          <img src={'./assets/images/github_ico.png'} alt='Visit my GitHub'></img>
-          <img src={'./assets/images/linkedin_ico.png'} alt='Visit my Linkedin'></img>
-          <img src={'./assets/images/gmail_ico.png'} alt='Contact me via Email'></img>
+          <img width="42" height="42" src={'./assets/svgs/github.svg'} alt='Visit my GitHub'></img>
+          <img width="42" height="42" src={'./assets/svgs/linkedin.svg'} alt='Visit my Linkedin'></img>
+          <img width="35" height="35" src={'./assets/svgs/resume.svg'} alt='Download my Resume'></img>
         </div>
       </div>
     </>
