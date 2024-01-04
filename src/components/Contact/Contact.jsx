980f253604a57fd248872f44f6390ad9899a805d
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-01-04 16:31:55 -0300

    Upload
    - changes made in order to achieve the correct looking of the header when resizing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,15 +3,14 @@ export default function Contact(){
   return(
     <>
       <div className='contact'>
-        <p>contact me</p>
         <div className='contact-images-container'>
           <a href='https://github.com/ignfer' target='_blank' rel='noreferrer'>
             <img width="42" height="42" src={'./assets/svgs/github.svg'} alt='Visit my GitHub'></img>
           </a>
-          <a href='www.linkedin.com/in/ignaciofernández' target='_blank' rel='noreferrer'>
+          <a href='https://linkedin.com/in/ignaciofernández' target='_blank' rel='noreferrer'>
             <img width="42" height="42" src={'./assets/svgs/linkedin.svg'} alt='Visit my Linkedin'></img>
           </a>
-          <a href='https://github.com/ignfer' target='_blank' rel='noreferrer'>
+          <a href='./documents/CV_IgnacioFernandez_esp_v01.pdf' target='_blank' rel='noreferrer'>
             <img width="35" height="35" src={'./assets/svgs/resume.svg'} alt='Download my Resume'></img>
           </a>
         </div>
