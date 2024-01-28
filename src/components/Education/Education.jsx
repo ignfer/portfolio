39de709824872c85b3f81dc64c10700fb38633a9
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-01-27 22:20:07 -0300

    Upload
    - reduced the size of various svgs
    - changed the color of the svgs to a static color
    - added more academic formation

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -9,16 +9,47 @@ export default function Education(){
         'description':'The Technologist Degree in Computer Science trains professionals with the skills to act in the development, implementation and administration of computer systems. A fundamental aspect is that although students use different technologies throughout their degree, the focus is on that when they graduate from it, they are able to adapt to working with new technologies that arise or that they have not studied at throughout the race. This aspect of training is fundamental, especially taking into account the constant evolution of tools and technologies in the IT field.',
         'organization':'UTEC - UDELAR',
         'imagepath':'./assets/svgs/utec.svg',
-        'status':'in course',
+        'status':'in course (first two years already completed)',
         'gpa':'4.6/5',
         'skill':[
           'C++',
           'Java',
+          'MySQL',
+          'Relational Databases',
           'Computer Networks',
           'Operative Systems',
           'Linux administration',
           'Assembly',
           'Python',
+        ]
+      },{
+        'title':'English certification',
+        'description':'The EF Standard English Test is a standardized test of the English language designed for non-native English speakers. It is the product of EF Education First, a global language training company, and a team of language assessment experts including Lyle Bachman, Mari Pearlman, and Ric Luecht. EF compares the EFSET\'s accuracy to the most widely used high stakes standardized English tests: TOEFL, IELTS, and Cambridge International Examinations.',
+        'organization':'EFSET',
+        'imagepath':'./assets/svgs/efset.svg',
+        'status':'completed',
+        'gpa':'78/100 - C2 Proficient',
+        'skill':[
+          'English',
+          'Language certification',
+        ]
+      },{
+        'title':'Technological Middle Education',
+        'description':'The EF Standard English Test is a standardized test of the English language designed for non-native English speakers. It is the product of EF Education First, a global language training company, and a team of language assessment experts including Lyle Bachman, Mari Pearlman, and Ric Luecht. EF compares the EFSET\'s accuracy to the most widely used high stakes standardized English tests: TOEFL, IELTS, and Cambridge International Examinations.',
+        'organization':'ANEP - UTU',
+        'imagepath':'./assets/svgs/anep.svg',
+        'status':'completed',
+        'gpa':'10/12',
+        'skill':[
+          'Java',
+          'MySQL',
+          'Relational Databases',
+          'Visual Basics',
+          'Arduino integration',
+          'Computer Networks',
+          'Operative Systems',
+          'Linux administration',
+          'PC Maintenance'
         ]
       }
     ]
