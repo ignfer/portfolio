cf47d64b65017a6b7218870d5697bc5f7b6ceaf6
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-02-27 23:28:19 -0300

    Upload
    - now the close and other buttons on the experience card modal are on top, with the modal title
    - removed some unnecessary margins and borders
    - now the images dont overflow a container, the experience card modal has just one scrollable container

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -15,7 +15,18 @@ export default function Modal({experience,modalId,closeModal}){
     <>
       <dialog className='modal' id={modalId}>
         <div className='modal-container'>
-          <h1 className='modal-title'>{experience.title}</h1>
+          
+          <div className='modal-header'>
+            <h1>{experience.title}</h1>
+            <div className='modal-buttons-wrapper'>
+              <button className='badge' onClick={() => redirect(experience.modalRepoRoute)}>Check the code</button>
+              
+              {experience.modalProjectRoute ? (<button className='badge' onClick={() => {redirect(experience.modalProjectRoute)}}>Visit project</button>) : (null)}
+              
+              <button className='badge' onClick={closeModal}>Close</button>
+            </div>
+          </div>
+
           <p>Developed Role: <strong>{experience.modalRole}</strong></p>
           
           <div className='modal-description-wrapper'>
@@ -23,17 +34,7 @@ export default function Modal({experience,modalId,closeModal}){
               return <p key={index}>{paragraph}</p>
             })}
           </div>
-          
-          <div className='modal-buttons-wrapper'>
-            <button className='badge' onClick={() => redirect(experience.modalRepoRoute)}>Check the code</button>
-            
-            {experience.modalProjectRoute ? (<button className='badge' onClick={() => {redirect(experience.modalProjectRoute)}}>Visit project</button>) : (null)}
-            
-            <button className='badge' onClick={closeModal}>Close</button>
-          </div>
-        </div>
-        
-        <div className='modal-images-wrapper'>
+
           <div className='modal-images'>
             {experience.cards.map((card,index) => {
               
