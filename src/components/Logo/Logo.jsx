68a4658a307e4290a6f692275389071de3b81e94
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-03-14 20:55:08 -0300

    Upload
    - improved the look and feel on the responsive version
    - improved the scrolleable areas and the general UX

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo(){
   return(
     <>
       <section className='header-title-section'>
-          <img src={'./assets/svgs/IF_logo.svg'}></img>
+          <img src={'/assets/svgs/IF_LOGO.svg'}></img>
           <h1>Ignacio<br></br>Fernández</h1>
           <h4>Junior Software<br></br>Developer</h4>
       </section>
