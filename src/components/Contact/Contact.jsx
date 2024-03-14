c2bbd601a6b1dfebe6a56c778acffe0248d56549
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-03-14 10:48:55 -0300

    Upload
    - now the text description on the contact section also redirects when clicked

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,35 @@
 import './Contact.css'
 export default function Contact(){
+  
+  function redirect(url){
+    window.open(url, '_blank', 'noopener,noreferrer');
+  }
+
+  const githubUrl = 'https://github.com/ignfer';
+  const linkedinUrl = 'https://linkedin.com/in/ignaciofernández';
+  const resumeUrl = './documents/CV_IgnacioFernandez_esp_v01.pdf';
+  
   return(
     <>
       <div className='contact'>
         <div className='contact-item-container'>
           <div className='contact-item'>
+            <a href={githubUrl} target='_blank' rel='noreferrer'>
             <p>GitHub | </p>
-            <a href='https://github.com/ignfer' target='_blank' rel='noreferrer'>
               <img width="25" height="25" src={'./assets/svgs/github.svg'} alt='Visit my GitHub'></img>
             </a>
           </div>
 
           <div className='contact-item'>
+            <a href={linkedinUrl} target='_blank' rel='noreferrer'>
             <p>Linkedin | </p>
-            <a href='https://linkedin.com/in/ignaciofernández' target='_blank' rel='noreferrer'>
               <img width="25" height="25" src={'./assets/svgs/linkedin.svg'} alt='Visit my Linkedin'></img>
             </a>
           </div>
 
           <div className='contact-item'>
+            <a href={resumeUrl} target='_blank' rel='noreferrer'>
             <p>Resume | </p>
-            <a href='./documents/CV_IgnacioFernandez_esp_v01.pdf' target='_blank' rel='noreferrer'>
               <img width="25" height="25" src={'./assets/svgs/resume.svg'} alt='Download my Resume'></img>
             </a>
           </div>
