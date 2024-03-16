eebfef647dfbd478d6ba76d7b0f9d243c540c686
Ignacio Fernández <nachofernandezuru@gmail.com>
2024-03-15 23:18:38 -0300

    Upload
    - preparation to publish v0.0.1 to prod

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo(){
   return(
     <>
       <section className='header-title-section'>
-          <img src={'/assets/svgs/IF_LOGO.svg'}></img>
+          <img src={'./IF_LOGO.svg'}></img>
           <h1>Ignacio<br></br>Fernández</h1>
           <h4>Junior Software<br></br>Developer</h4>
       </section>
