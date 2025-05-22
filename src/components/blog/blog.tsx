9e205f6ea6570ae20063870e7fe72fb692a109af
IgnFer <nachofernandezuru@gmail.com>
2025-05-21 23:05:32 -0300

    migration: yarn -> npm

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -1,19 +1,19 @@
 import './blog.css';
 
-import Markdown from 'react-markdown';
+/*import Markdown from 'react-markdown';
 
 import markdownContent from '../../docs/blog_posts/01_how_dhh_and_37signals_force_me_to_update_my_portfolio.md?raw';
-import MarkdownContainer from '../markdown/markdown-container';
+import MarkdownContainer from '../markdown/markdown-container';*/
 
 export default function Blog() {
 
   return (
     <>
-      <div className='blog flex justify-center'>
+      {/*<div className='blog flex justify-center'>
         <MarkdownContainer>
           <Markdown>{markdownContent}</Markdown>
         </MarkdownContainer>
-      </div>
+      </div>*/}
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
-import {defineConfig} from 'vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    hmr: {
-      overlay: true,
-    },
-  },
-  base: '/portfolio/'
-});
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+})
