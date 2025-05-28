b91b90631850fe40acf9d3a020b8e9834df7bb0e
IgnFer <nachofernandezuru@gmail.com>
2025-05-27 23:58:58 -0300

    fix: deploy
    - added missing dependencies
    - removed unused imports

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,16 @@ import Main from './components/main/main.tsx';
 
 import '@mantine/core/styles.css';
 import '@mantine/spotlight/styles.css';
-import '@mantine/modals/styles.css';
-import '@mantine/nprogress/styles.css';
-import '@mantine/carousel embla-carousel/styles.css';
-import '@mantine/code-highlight/styles.css';
 
 import {MantineProvider} from '@mantine/core';
 
 function App() {
   return (
     <Router>
-    <MantineProvider>
-      <Header/>
-      <Main/>
-    </MantineProvider>
+      <MantineProvider>
+        <Header/>
+        <Main/>
+      </MantineProvider>
     </Router>
   );
 }
