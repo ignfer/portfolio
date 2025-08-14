6bcc7b0fd4283f19cc5e95291ddd5d6fd864a034
IgnFer <nachofernandezuru@gmail.com>
2025-08-13 21:28:01 -0300

    add: open graph fix
    - attempt to fix og banner image
    - attempt to fix crawlers getting stuck in 'redirecting to /home'

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,9 +12,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ignfer.com',
-    redirects: {
-      '/': '/home'
-    },
     base: "/",
     vite: {
         resolve: {
